c352eb8cd2b026303a3bd1f9b605791a52c66160
mariarizzo <mrizzo@bgsu.edu>
2019-05-30 12:22:40 -0400

    merged to dcov2d

--- src/energy_init.c
+++ src/energy_init.c
@@ -24,6 +24,10 @@ extern SEXP _energy_U_center(SEXP);
 extern SEXP _energy_U_product(SEXP, SEXP);
 extern SEXP _energy_Btree_sum(SEXP, SEXP);
 extern SEXP _energy_kgroups_start(SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _energy_calc_dist(SEXP);
+extern SEXP _energy_calcH2d(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _energy_dCov2(SEXP, SEXP, SEXP);
+extern SEXP _energy_dCov2stats(SEXP, SEXP, SEXP);
 
 static const R_CMethodDef CEntries[] = {
   {"dCOV",         (DL_FUNC) &dCOV,         7},
@@ -46,6 +50,10 @@ static const R_CallMethodDef CallEntries[] = {
   {"_energy_U_product",      (DL_FUNC) &_energy_U_product,     2},
   {"_energy_Btree_sum",      (DL_FUNC) &_energy_Btree_sum,     2},
   {"_energy_kgroups_start",  (DL_FUNC) &_energy_kgroups_start, 5},
+  {"_energy_calc_dist",      (DL_FUNC) &_energy_calc_dist,     1},
+  {"_energy_calcH2d",        (DL_FUNC) &_energy_calcH2d,       7},
+  {"_energy_dCov2",          (DL_FUNC) &_energy_dCov2,         3},
+  {"_energy_dCov2stats",     (DL_FUNC) &_energy_dCov2stats,    3},
   {NULL, NULL, 0}
 };
 
