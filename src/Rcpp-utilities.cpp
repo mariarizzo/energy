3a9eec162c5519e56ef1afc829c707a1189a6b72
mariarizzo <mrizzo@bgsu.edu>
2019-05-30 09:50:12 -0400

    .calc_dist added

---
+++ src/Rcpp-utilities.cpp
@@ -0,0 +1,26 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export(.calc_dist)]]
+NumericMatrix calc_dist(NumericMatrix x) {
+  int n = x.nrow(), d = x.ncol(), i, j, k;
+  double dsum, dk;
+  NumericMatrix Dx(n, n);
+  for (i = 0; i < n; i++) {
+    for (j = i; j < n; j++) {
+      if (i == j) {
+        Dx(i, i) = 0.0; 
+      } else {
+        dsum = 0.0;
+        for (k = 0; k < d; k++) {
+          dk = x(i,k) - x(j,k);
+          dsum += dk * dk;
+        }
+        Dx(i, j) = sqrt(dsum);
+        Dx(j, i) = sqrt(dsum);
+      }
+    }
+  }
+  return Dx;
+}
+
