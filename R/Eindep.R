b27b56f48d7ca2c10197cf5a7578e9728e1ed2a7
Maria Rizzo <mrizzo@bgsu.edu>
2019-12-07 12:18:23 -0500

    Add files via upload

--- R/Eindep.R
+++ R/Eindep.R
@@ -78,20 +78,3 @@ function(x, y, R) {
 }
 
 
-
-
-indep.e<-
-function(x, y) {
-    # energy statistic for multivariate independence (deprecated)
-    .Defunct(new = "mvI", package = "energy")
-}
-
-
-indep.etest<-
-function(x, y, R) {
-    # energy test for multivariate independence (deprecated)
-    .Defunct(new = "indep.test", package = "energy",
-        msg = "indep.etest removed; use indep.test with method mvI.")
-}
-
-
