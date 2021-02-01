b70d83ebf8c1caca0cff95c41dc819faaed16174
mariarizzo <mrizzo@bgsu.edu>
2021-01-31 20:11:41 -0500

    add energy Poisson test

--- R/Epoisson.R
+++ R/Epoisson.R
@@ -1,16 +1,28 @@
-poisson.mtest <-
-function(x, R) {
-    # parametric bootstrap mean distance test of Poisson distribution
+poissonGOF.test <-
+function(x, R, method=c("M","E")) {
+    # parametric bootstrap energy tests of Poisson distribution
+    # poisson.e is the energy GOF statistic
+    # poisson.m is the mean distance statistic
     n <- length(x)
     lambda <- mean(x)
-    bootobj <- boot::boot(x, statistic = poisson.m, R = R, sim = "parametric",
-            ran.gen = function(x, y) {rpois(n, lambda)})
+    method <- match.arg(method)
+    if (method == "M") {
+      bootobj <- boot::boot(x, statistic = poisson.m, R = R, 
+                   sim = "parametric",
+                   ran.gen = function(x, y) {rpois(n, lambda)})
+      test <- "Mean distance test of Poisson distribution"
+    } else {
+      bootobj <- boot::boot(x, statistic = poisson.e, R = R, 
+                            sim = "parametric",
+                            ran.gen = function(x, y) {rpois(n, lambda)})
+      test <- "Energy test of Poisson distribution"
+    }
     if (R > 0)
       p <- 1 - mean(bootobj$t < bootobj$t0) else p <- NA
     names(bootobj$t0) <- "test statistic"
     names(lambda) <- "mean"
     e <- list(
-        method = paste("Mean distance test of Poisson distribution", sep = ""),
+        method = test,
         statistic = bootobj$t0,
         p.value = p,
         data.name = paste("sample size ", n, ", replicates ", R, sep=""),
@@ -19,7 +31,11 @@ function(x, R) {
     e
 }
 
-poisson.m<-
+poisson.mtest <- function(x, R) {
+  return(poissonGOF.test(x, R=R, method="M"))
+}
+
+poisson.m <-
 function(x) {
     # mean distance statistic for Poissonity
     n <- length(x)
@@ -31,3 +47,27 @@ function(x) {
             PACKAGE = "energy")$stat
     e
 }
+
+poisson.e <-
+function(x) {
+  # energy GOF statistic for Poissonity
+  lambda <- mean(x)
+  n <- length(x)
+  
+  ## E|y-X| for X Poisson(lambda) (vectorized)
+  Px <- ppois(x, lambda)
+  Px1 <- ppois(x-1, lambda)
+  meanvec <- 2*x*Px - 2*lambda*Px1 + lambda - x
+  
+  ## second mean E|X-X'|
+  a <- 2 * lambda
+  EXX <- a * exp(-a) * (besselI(a, 0) + besselI(a, 1))
+  
+  ## third mean = sum_{i,j} |x_i - x_j| / n^2
+  K <- seq(1 - n, n - 1, 2)
+  y <- sort(x)
+  meanxx <- 2 * sum(K * y) / n^2
+  
+  e <- n * (2 * mean(meanvec) - EXX - meanxx)
+}
+
