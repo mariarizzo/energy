8dc3fff5714c81d7dd272fcc00424495a247f847
mariarizzo <mrizzo@bgsu.edu>
2021-02-19 15:39:21 -0500

    more Poisson test updates

--- R/Epoisson.R
+++ R/Epoisson.R
@@ -1,33 +1,42 @@
-poissonGOF.test <-
-function(x, R) {
-  # parametric bootstrap energy tests of Poisson distribution
+poisson.tests <-
+function(x, R, test="all") {
+  # parametric bootstrap tests of Poisson distribution
   # poisson.e is the energy GOF statistic
   # poisson.m is the mean distance statistic
-  method <- c("Poisson M-test", "Poisson E-test")
-  stat <- .poisson.stats
-
+  # (not related to the test stats::poisson.test)
   if (any(!is.integer(x)) || any(x < 0)) {
-      warning("sample must be non-negative integers")
-      return(NULL)
-    }
+    warning("sample must be non-negative integers")
+    return(NULL)
+  }
+  test <- casefold(test)
+  poisson.stats <- function(x) {
+    c(poisson.m(x), poisson.e(x))
+  }
+  stat <- switch(test,
+                 "m" = poisson.m,
+                 "e" = poisson.e,
+                 poisson.stats)
+  
+  method <- switch(test, m="M", e="E", c("M","E"))
+  method <- paste(method, "-test", sep="")
   n <- length(x)
   lambda <- mean(x)
   if (missing(R)) {
     R <- 0
     message("Specify R > 0 replicates for MC test")
   }
-    
+
   bootobj <- boot::boot(x, statistic = stat, R = R, 
                    sim = "parametric",
                    ran.gen = function(x, y) {rpois(n, lambda)})
-
+  
+  N <- length(bootobj$t0)
+  p <- rep(NA, times=N)
   if (R > 0) {
-    p1 <- 1 - mean(bootobj$t[,1] < bootobj$t0[1])
-    p2 <- 1 - mean(bootobj$t[,2] < bootobj$t0[2])
-    p <- c(p1, p2)
-    } else {
-      p <- c(NA, NA)
+    for (i in 1:N) {
+    p[i] <- 1 - mean(bootobj$t[,i] < bootobj$t0[i])
     }
+  }
   
   # a data frame, not an htest object  
   # comparable to broom::tidy on an htest object
@@ -36,61 +45,39 @@ function(x, R) {
   return(RVAL)
 }
 
-poisson.mtest <- function(x, R) {
-  # for backward compatibility
-  poissonM.test(x, R)
-}
-
-poissonM.test <- 
-  function(x, R) {
-    # parametric bootstrap mean distance test of Poisson distribution
-    if (any(!is.integer(x)) || any(x < 0)) {
-      warning("sample must be non-negative integers")
-      return(NULL)
-    }
-    n <- length(x)
-    lambda <- mean(x)
-    bootobj <- boot::boot(x, statistic = poisson.m, R = R, sim = "parametric",
-                          ran.gen = function(x, y) {rpois(n, lambda)})
-    if (R > 0)
-      p <- 1 - mean(bootobj$t < bootobj$t0) else p <- NA
-    names(bootobj$t0) <- "test statistic"
-    names(lambda) <- "mean"
+poisson.mtest <-
+function(x, R=NULL) {
+  if (is.null(R)) R <- 0
+  rval <- poisson.tests(x, R, test="M")
+  DNAME <- paste(deparse1(substitute(x)), "replicates: ", R)
+  stat <- rval$statistic
+  names(stat) <- "M"
     e <- list(
       method = paste("Poisson M-test", sep = ""),
-      statistic = bootobj$t0,
-      p.value = p,
-      data.name = paste("sample size ", n, ", replicates ", R, sep=""),
-      estimate = lambda)
+      statistic = stat,
+      p.value = rval$p.value,
+      data.name = DNAME,
+      estimate = rval$estimate)
     class(e) <- "htest"
     e
-  }
+}
 
-
-poissonE.test <-
-  function(x, R) {
-    # parametric bootstrap mean distance test of Poisson distribution
-    if (any(!is.integer(x)) || any(x < 0)) {
-      warning("sample must be non-negative integers")
-      return(NULL)
-    }
-    n <- length(x)
-    lambda <- mean(x)
-    bootobj <- boot::boot(x, statistic = poisson.e, R = R, sim = "parametric",
-                          ran.gen = function(x, y) {rpois(n, lambda)})
-    if (R > 0)
-      p <- 1 - mean(bootobj$t < bootobj$t0) else p <- NA
-    names(bootobj$t0) <- "test statistic"
-    names(lambda) <- "mean"
+poisson.etest <- 
+function(x, R=NULL) {
+  if (is.null(R)) R <- 0
+    rval <- poisson.tests(x, R, test="E")
+    DNAME <- paste(deparse1(substitute(x)), "replicates: ", R)
+    stat <- rval$statistic
+    names(stat) <- "E"
     e <- list(
       method = paste("Poisson E-test", sep = ""),
-      statistic = bootobj$t0,
-      p.value = p,
-      data.name = paste("sample size ", n, ", replicates ", R, sep=""),
-      estimate = lambda)
+      statistic = stat,
+      p.value = rval$p.value,
+      data.name = paste("replicates: ", R,  sep=""),
+      estimate = rval$estimate)
     class(e) <- "htest"
     e
-  }
+}
 
 poisson.m <-
 function(x) {
@@ -126,9 +113,7 @@ function(x) {
   y <- sort(x)
   meanxx <- 2 * sum(K * y) / n^2
   
-  e <- n * (2 * mean(meanvec) - EXX - meanxx)
+  n * (2 * mean(meanvec) - EXX - meanxx)
 }
 
-.poisson.stats <- function(x, lambda) {
-  c(poisson.m(x), poisson.e(x))
-}
+
